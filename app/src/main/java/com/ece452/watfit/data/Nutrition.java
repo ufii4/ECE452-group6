3e73c18b6969e10437333d28a5d69b2fe186618e
Alex Xie <pc.xie00@gmail.com>
2023-06-11 20:34:08 -0400

    Added remote spoonacular data source

---
+++ app/src/main/java/com/ece452/watfit/data/Nutrition.java
@@ -0,0 +1,11 @@
+package com.ece452.watfit.data;
+
+public class Nutrition {
+    public class Nutrient {
+        public String name;
+        public String unit;
+        public double amount;
+    }
+
+    public Nutrient nutrients[];
+}
