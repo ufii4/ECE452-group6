362ef26e76044ea88ef951afb976ad1fe80ebac7
Xu Tao <x29tao@uwaterloo.ca>
2023-06-11 20:01:47 -0400

    Improve the layout and add back button to come back to previous page, fixed bug for re entering Calorie page.

--- app/src/main/java/com/ece452/watfit/ui/tracker/CalorieFragment.java
+++ app/src/main/java/com/ece452/watfit/ui/tracker/CalorieFragment.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.ece452.watfit.R;
@@ -21,13 +24,29 @@ public class CalorieFragment extends Fragment {
                 new ViewModelProvider(this).get(TrackerViewModel.class);
 
         binding = FragmentCalorieIntakeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();return root;
+        View root = binding.getRoot();
+
+        Button backButton = root.findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            TrackerFragment trackerFragment = new TrackerFragment();
+
+            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+            fragmentTransaction.replace(R.id.nav_host_fragment_activity_main, trackerFragment);
+            fragmentTransaction.addToBackStack(null);  // This line is optional, and will add the transaction to the back stack
+
+            fragmentTransaction.commit();
+            }
+        });
+
+        return root;
     }
 
     @Override
     public void onDestroyView() {
-        ViewGroup mContainer = (ViewGroup) getActivity().findViewById(R.id.nav_host_fragment_activity_main);
-        mContainer.removeAllViews();
         super.onDestroyView();
         binding = null;
     }
