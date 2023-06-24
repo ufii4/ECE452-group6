d1a9e2d225c5776c3f6d2483cc0f57985fe95968
Fred Hu <p23hu@uwaterloo.com>
2023-06-24 14:01:47 -0400

    Add remaining videoviews, removed the sample test, updated TimeLog

--- TimeLog.md
+++ TimeLog.md
@@ -29,3 +29,5 @@
 | 2023/06/18 |           |            |        |          | 4           |           | FitnessGaolActivity UI Add cards functionality. The remove card functionality has bug to be fixed                         |
 | 2023/06/18 |           |            |        | 4        |             |           | Fixed FitnessGoalActivity, wired in data with firestore                                                                   |
 | 2023/06/23 |           |            |        |         |             | 10           |Learn to use and integrate the spoonacular API; learn the networking in Android; implement the backend of recipe generator; the rest of ui of recipe generator is in progress
+| 2023/06/23 | 3         | 0          | 0      | 0        | 0           | 0         | Working on fixing the videoviews in rec. list
+| 2023/06/23 | 2         | 0          | 0      | 0        | 0           | 0         | Fixed the functionality for videoviews in the rec list, adding the remaining videoviews
--- app/src/main/java/com/ece452/watfit/ui/home/HomeFragment.java
+++ app/src/main/java/com/ece452/watfit/ui/home/HomeFragment.java
@@ -21,7 +21,8 @@ public class HomeFragment extends Fragment {
     private FragmentHomeBinding binding;
     // Your Video URL
     String videoUrl = "https://media.geeksforgeeks.org/wp-content/uploads/20201217192146/Screenrecorder-2020-12-17-19-17-36-828.mp4?_=1";
-
+    String videoUrl2 = "https://www.youtube.com/watch?v=i9BupglHdtM";
+    String videoUrl3 = "https://www.youtube.com/embed/zWh3CShX_do";
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel =
@@ -30,19 +31,51 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        //final TextView textView = binding.textHome;
+        //homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
-        // Video Code Start Here
-        VideoView videoView = (VideoView) root.findViewById(R.id.rec_v1);
+        // Video 1
+        VideoView videoView1 = (VideoView) root.findViewById(R.id.rec_v1);
         Uri uri = Uri.parse(videoUrl);
-        videoView.setVideoURI(uri);
+        videoView1.setVideoURI(uri);
         MediaController mediaController = new MediaController(root.getContext());
-        mediaController.setAnchorView(videoView);
-        mediaController.setMediaPlayer(videoView);
-        videoView.setMediaController(mediaController);
-        videoView.start();
-        //Video Code Ends Here
+        mediaController.setAnchorView(videoView1);
+        mediaController.setMediaPlayer(videoView1);
+        videoView1.setMediaController(mediaController);
+        videoView1.start();
+        //Video 2
+        VideoView videoView2 = (VideoView) root.findViewById(R.id.rec_v2);
+        Uri uri2 = Uri.parse(videoUrl);
+        videoView2.setVideoURI(uri2);
+        mediaController.setAnchorView(videoView2);
+        mediaController.setMediaPlayer(videoView2);
+        videoView2.setMediaController(mediaController);
+        videoView2.start();
+        //Video 3
+        VideoView videoView3 = (VideoView) root.findViewById(R.id.rec_v3);
+        Uri uri3 = Uri.parse(videoUrl);
+        videoView3.setVideoURI(uri3);
+        mediaController.setAnchorView(videoView3);
+        mediaController.setMediaPlayer(videoView3);
+        videoView3.setMediaController(mediaController);
+        videoView3.start();
+        //Video 4
+        VideoView videoView4 = (VideoView) root.findViewById(R.id.rec_v4);
+        Uri uri4 = Uri.parse(videoUrl);
+        videoView4.setVideoURI(uri4);
+        mediaController.setAnchorView(videoView4);
+        mediaController.setMediaPlayer(videoView4);
+        videoView4.setMediaController(mediaController);
+        videoView4.start();
+        //Video 5
+        VideoView videoView5 = (VideoView) root.findViewById(R.id.rec_v5);
+        Uri uri5 = Uri.parse(videoUrl);
+        videoView5.setVideoURI(uri2);
+        mediaController.setAnchorView(videoView5);
+        mediaController.setMediaPlayer(videoView5);
+        videoView5.setMediaController(mediaController);
+        videoView5.start();
+        //Videos end
         return root;
     }
 
