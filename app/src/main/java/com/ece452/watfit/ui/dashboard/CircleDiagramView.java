bfc84e4d271e822b3f271248c38da89b62ec8a5a
Xu Tao <x29tao@uwaterloo.ca>
2023-07-16 09:56:47 -0400

    generate suggestions and health score

--- app/src/main/java/com/ece452/watfit/ui/dashboard/CircleDiagramView.java
+++ app/src/main/java/com/ece452/watfit/ui/dashboard/CircleDiagramView.java
@@ -8,29 +8,38 @@ import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 public class CircleDiagramView extends View {
     private int score;
 
     private Paint circlePaint;
     private Paint fillPaint;
     private Paint textPaint;
+    private List<Double> dailyCalorie = new ArrayList<>();
+    private List<Double> exerciseCalorie = new ArrayList<>();
 
-    public CircleDiagramView(Context context) {
-        super(context);
-        init();
-        setScore(80);
-    }
+    private static final int MAX_SCORE = 100;
+    private static final double MAX_BMI = 30.0;
+    private static final double MAX_WHR_MALE = 1.0;
+    private static final double MAX_WHR_FEMALE = 0.9;
 
     public CircleDiagramView(Context context, AttributeSet attrs) {
         super(context, attrs);
         init();
-        setScore(80);
-    }
-
-    public CircleDiagramView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init();
-        setScore(80);
+        fetchData();
     }
 
     private void init() {
@@ -48,6 +57,183 @@ public class CircleDiagramView extends View {
         textPaint.setTextSize(40f);
         textPaint.setTextAlign(Paint.Align.CENTER);
         textPaint.setTypeface(Typeface.DEFAULT_BOLD);
+    }
+
+    private void fetchData() {
+        // Get the current date
+        Calendar calendar = Calendar.getInstance();
+        Date currentDate = calendar.getTime();
+
+        // Calculate the date 7 days ago
+        calendar.add(Calendar.DAY_OF_YEAR, -7);
+        Date sevenDaysAgo = calendar.getTime();
+
+        String uid = FirebaseAuth.getInstance().getUid();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("users").document(uid);
+
+        // Fetch calorie logs within 7 days
+        Task<QuerySnapshot> calorieTask = docRef.collection("calorieLogs")
+                .whereGreaterThanOrEqualTo("date", sevenDaysAgo)
+                .whereLessThanOrEqualTo("date", currentDate)
+                .get();
+
+        // Fetch exercise logs within 7 days
+        Task<QuerySnapshot> exerciseTask = docRef.collection("exerciseLogs")
+                .whereGreaterThanOrEqualTo("date", sevenDaysAgo)
+                .whereLessThanOrEqualTo("date", currentDate)
+                .get();
+
+        // Fetch user information
+        Task<DocumentSnapshot> userTask = docRef.get();
+
+        // Combine all tasks
+        Task<List<Object>> combinedTask = Tasks.whenAllSuccess(calorieTask, exerciseTask, userTask)
+                .addOnSuccessListener(new OnSuccessListener<List<Object>>() {
+                    @Override
+                    public void onSuccess(List<Object> results) {
+                        dailyCalorie = extractCalorieLogs(results.get(0));
+                        exerciseCalorie = extractCalorieLogs(results.get(1));
+
+                        DocumentSnapshot userSnapshot = (DocumentSnapshot) results.get(2);
+                        int age = userSnapshot.getDouble("age").intValue();
+                        int height = userSnapshot.getDouble("height").intValue();
+                        int hip = userSnapshot.getDouble("hip").intValue();
+                        int waist = userSnapshot.getDouble("waist").intValue();
+                        int weight = userSnapshot.getDouble("weight").intValue();
+                        String gender = userSnapshot.getString("gender");
+
+                        // Calculate the score based on the fetched data
+                        calculateScore(dailyCalorie, exerciseCalorie, age, height, hip, waist, weight, gender);
+                        setScore(score);
+                    }
+                });
+
+        // Execute the combined task
+        combinedTask.addOnCompleteListener(task -> {
+            // Handle completion if needed
+        });
+    }
+
+    private List<Double> extractCalorieLogs(Object result) {
+        List<Double> calorieLogs = new ArrayList<>();
+        if (result instanceof QuerySnapshot) {
+            QuerySnapshot querySnapshot = (QuerySnapshot) result;
+            for (DocumentSnapshot documentSnapshot : querySnapshot.getDocuments()) {
+                Double calorie = documentSnapshot.getDouble("dailyCalorie");
+                if (calorie != null) {
+                    calorieLogs.add(calorie);
+                }
+            }
+        }
+        return calorieLogs;
+    }
+
+    private void calculateScore(List<Double> dailyCalorie, List<Double> exerciseCalorie, int age, int height, int hip, int waist, int weight, String gender) {
+        double calorieScore = calculateCalorieScore(dailyCalorie);
+        double exerciseScore = calculateExerciseScore(exerciseCalorie, gender, weight);
+        double bodyScore = calculateBodyScore(age, height, hip, waist, weight, gender);
+
+        // You can customize the weights of each score to fit your specific criteria
+        double totalScore = calorieScore + exerciseScore + bodyScore;
+
+        // Normalize the total score to a value between 0 and 100
+        double normalizedScore = (totalScore / (3 * MAX_SCORE)) * 100;
+
+        score = (int) Math.round(normalizedScore);
+    }
+
+    private double calculateCalorieScore(List<Double> dailyCalorie) {
+        // Calculate the average daily calorie intake
+        double totalCalorie = 0;
+        for (Double calorie : dailyCalorie) {
+            totalCalorie += calorie;
+        }
+        double averageCalorie = totalCalorie / dailyCalorie.size();
+
+        // Define thresholds for low and high calorie intake
+        double lowThreshold = 1500;
+        double highThreshold = 2500;
+
+        // Calculate the score based on the average daily calorie intake
+        if (averageCalorie < lowThreshold) {
+            return 0;
+        } else if (averageCalorie > highThreshold) {
+            return MAX_SCORE;
+        } else {
+            return (averageCalorie - lowThreshold) / (highThreshold - lowThreshold) * MAX_SCORE;
+        }
+    }
+
+    private double calculateExerciseScore(List<Double> exerciseCalorie, String gender, int weight) {
+        // Calculate the total exercise calorie burned
+        double totalCalorie = 0;
+        for (Double calorie : exerciseCalorie) {
+            totalCalorie += calorie;
+        }
+
+        // Define the target goals for exercise calorie burn based on gender and weight
+        double targetGoalMale = weight * 20;     // Adjust the multiplier as needed
+        double targetGoalFemale = weight * 18;   // Adjust the multiplier as needed
+
+        // Calculate the percentage of exercise calories burned compared to the target goal
+        double percentage;
+        if (gender.equalsIgnoreCase("Male")) {
+            percentage = (totalCalorie / targetGoalMale) * 100;
+        } else {
+            percentage = (totalCalorie / targetGoalFemale) * 100;
+        }
+
+        // Assign a score based on the percentage
+        if (percentage >= 100) {
+            return MAX_SCORE;
+        } else {
+            return (percentage / 100) * MAX_SCORE;
+        }
+    }
+
+    private double calculateBodyScore(int age, int height, int hip, int waist, int weight, String gender) {
+        // Calculate BMI score
+        double bmi = calculateBMI(height, weight);
+        double bmiScore = calculateBMIScore(bmi);
+
+        // Calculate WHR score
+        double whr = calculateWHR(hip, waist, gender);
+        double whrScore = calculateWHRScore(whr, gender);
+
+        // Calculate the total body score
+        return (bmiScore + whrScore) / 2;
+    }
+
+    private double calculateBMI(int height, int weight) {
+        double heightInMeters = height / 100.0;
+        return weight / (heightInMeters * heightInMeters);
+    }
+
+    private double calculateBMIScore(double bmi) {
+        if (bmi < MAX_BMI) {
+            return MAX_SCORE;
+        } else {
+            return (1 - (bmi - MAX_BMI) / MAX_BMI) * MAX_SCORE;
+        }
+    }
+
+    private double calculateWHR(int hip, int waist, String gender) {
+        double whr = (double) waist / hip;
+        if (gender.equalsIgnoreCase("Male")) {
+            return whr;
+        } else {
+            return 1 / whr;
+        }
+    }
+
+    private double calculateWHRScore(double whr, String gender) {
+        double maxWhr = (gender.equalsIgnoreCase("Male")) ? MAX_WHR_MALE : MAX_WHR_FEMALE;
+        if (whr <= maxWhr) {
+            return MAX_SCORE;
+        } else {
+            return (1 - (whr - maxWhr) / maxWhr) * MAX_SCORE;
+        }
     }
 
     public void setScore(int score) {
--- app/src/main/java/com/ece452/watfit/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/ece452/watfit/ui/dashboard/DashboardFragment.java
@@ -55,12 +55,17 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.Arrays;
 
 import javax.inject.Inject;
 
 @AndroidEntryPoint
 public class DashboardFragment extends Fragment {
     private FragmentDashboardBinding binding;
+    private TextView suggestion1;
+    private TextView suggestion2;
+    private TextView suggestion3;
     private Timestamp localDate = null;
     private List<Double> dailyCalorie = new ArrayList<>();
     private List<Double> exerciseCalorie = new ArrayList<>();
@@ -83,6 +88,113 @@ public class DashboardFragment extends Fragment {
 
         //Add date to entries
         List<String> dateEntries = new ArrayList<>();
+
+        List<String> eatSuggestions = new ArrayList<>(Arrays.asList(
+            "Try incorporating more fruits and vegetables into your meals for added nutrients.",
+            "Experiment with different spices and herbs to enhance the flavors of your dishes.",
+            "Include a source of protein in each of your meals, such as lean meats, tofu, or legumes.",
+            "Opt for whole grain options like brown rice or whole wheat bread for added fiber.",
+            "Don't forget to stay hydrated by drinking plenty of water throughout the day.",
+            "Try to limit processed foods and opt for fresh, whole foods instead.",
+            "Consider trying a new recipe or cooking technique to keep meals exciting.",
+            "Include healthy fats in your diet, such as avocados, nuts, and olive oil.",
+            "Balance your plate with a variety of colors by including different types of fruits and vegetables.",
+            "Practice portion control to ensure you're eating an appropriate amount for your needs.",
+            "Enjoy a balanced breakfast to kickstart your day with energy and nutrients.",
+            "Pack nutritious snacks like cut-up veggies or trail mix to have on hand when hunger strikes.",
+            "Try to limit sugary drinks and opt for water, herbal tea, or infused water instead.",
+            "Don't skip meals, as it can lead to overeating later on.",
+            "Listen to your body's hunger and fullness cues to guide your eating habits.",
+            "Include probiotic-rich foods like yogurt or sauerkraut to support gut health.",
+            "Plan and prepare your meals ahead of time to avoid unhealthy food choices on busy days.",
+            "Choose lean sources of protein, such as fish or skinless poultry, for heart-healthy options.",
+            "Enjoy a variety of different cuisines to explore new flavors and ingredients.",
+            "Engage in mindful eating by savoring each bite and paying attention to your body's signals.",
+            "Opt for homemade meals whenever possible, so you have control over the ingredients used.",
+            "Try to include a source of calcium, such as dairy products or fortified plant-based alternatives.",
+            "Consider reducing your sodium intake by seasoning your meals with herbs and spices instead.",
+            "Include high-fiber foods like legumes, whole grains, and vegetables to support digestion.",
+            "Experiment with meatless meals by incorporating plant-based proteins like tofu or lentils.",
+            "Enjoy occasional treats in moderation to satisfy cravings without overindulging.",
+            "Stay mindful of your eating environment by sitting down at a table and minimizing distractions.",
+            "Incorporate healthy snacks like fresh fruit, nuts, or yogurt for a balanced diet.",
+            "Practice gratitude for the food you eat and appreciate the nourishment it provides.",
+            "Consult with a registered dietitian for personalized dietary recommendations and guidance."
+        ));
+        List<String> exerciseSuggestions = new ArrayList<>(Arrays.asList(
+            "Embrace the joy of movement and dance your way to fitness.",
+            "Discover the exhilarating world of outdoor cycling for a refreshing workout.",
+            "Strengthen your core with invigorating Pilates sessions.",
+            "Engage in uplifting group workouts to foster a sense of community.",
+            "Unleash your inner athlete with a challenging HIIT routine.",
+            "Find solace in serene yoga sessions that nurture your mind and body.",
+            "Take a plunge into the pool for a refreshing and low-impact exercise experience.",
+            "Power up your day with an energizing morning jog or run.",
+            "Embrace the art of calisthenics to sculpt and tone your body.",
+            "Challenge yourself with martial arts for a dynamic and empowering workout.",
+            "Channel your inner child and jump rope your way to fitness and fun.",
+            "Explore the beauty of nature through hiking for a revitalizing workout.",
+            "Push your limits and conquer obstacles with thrilling obstacle course training.",
+            "Engage in mindfulness and strengthen your muscles with Tai Chi.",
+            "Get your heart pumping and body moving with exhilarating Zumba classes.",
+            "Bask in the endorphin rush of a high-energy kickboxing workout.",
+            "Boost your flexibility and balance with the graceful practice of ballet-inspired exercises.",
+            "Discover the benefits of meditation and movement with gentle Tai Chi flows.",
+            "Indulge in the freedom of aerial yoga and find tranquility while suspended in the air.",
+            "Find your rhythm and groove with rhythmic gymnastics-inspired workouts.",
+            "Experience the thrill of rock climbing and challenge your strength and agility.",
+            "Tone and sculpt your body with the power of barre workouts.",
+            "Dive into the world of aquatic fitness and enjoy a refreshing and joint-friendly exercise.",
+            "Embrace the uplifting beats of a cardio dance class to get your heart rate up.",
+            "Break a sweat and boost your endurance with a high-intensity indoor cycling class.",
+            "Unleash your inner yogi with a challenging and empowering hot yoga session.",
+            "Rejuvenate your mind and body with a peaceful and meditative nature walk.",
+            "Strengthen your muscles and improve your posture with Pilates reformer workouts.",
+            "Embrace the serenity of paddleboarding and work your core while connecting with nature.",
+            "Get your groove on and burn calories with fun-filled dance workouts like salsa or hip-hop."
+        ));
+        ArrayList<String> healthySuggestions = new ArrayList<>(Arrays.asList(
+            "Embrace the joy of fresh fruits and vegetables.",
+            "Engage in invigorating outdoor activities for a vibrant lifestyle.",
+            "Prioritize a good night's sleep for optimal well-being.",
+            "Savor the flavors of wholesome, homemade meals.",
+            "Cultivate mindfulness and find peace in the present moment.",
+            "Stay hydrated and let water be your refreshing companion.",
+            "Enjoy the benefits of regular exercise and feel the energy surge.",
+            "Discover new recipes that nourish your body and tantalize your taste buds.",
+            "Surround yourself with positive, supportive people who uplift your spirits.",
+            "Take time to relax and rejuvenate through self-care activities.",
+            "Explore the world of herbal teas for soothing and healing experiences.",
+            "Practice gratitude daily to foster a positive mindset.",
+            "Challenge yourself to try new, nutritious foods and expand your palate.",
+            "Breathe deeply and feel the tranquility that flows within.",
+            "Engage in creative endeavors that bring you joy and fulfillment.",
+            "Share laughter with loved ones and embrace the healing power of humor.",
+            "Step outside and bask in the warm embrace of sunlight.",
+            "Find a physical activity you love and make it a part of your routine.",
+            "Nourish your mind with books, podcasts, and stimulating conversations.",
+            "Indulge in the sweetness of life while maintaining balance.",
+            "Take breaks from screens and reconnect with the world around you.",
+            "Set achievable goals and celebrate every milestone along the way.",
+            "Explore different forms of meditation to find inner peace.",
+            "Connect with nature and find solace in its beauty.",
+            "Practice acts of kindness and experience the joy of giving.",
+            "Embrace the power of positive affirmations to uplift your spirits.",
+            "Seek professional help and guidance when needed for holistic well-being.",
+            "Engage in hobbies that challenge your mind and spark your curiosity.",
+            "Maintain a clean and organized living space for a calm and focused mind.",
+            "Embrace a healthy work-life balance and prioritize self-care."
+        ));
+        Random random = new Random();
+        suggestion1 = root.findViewById(R.id.text_suggestion1);
+        int randomNumber = random.nextInt(30);
+        suggestion1.setText(eatSuggestions.get(randomNumber));
+        suggestion2 = root.findViewById(R.id.text_suggestion2);
+        randomNumber = random.nextInt(30);
+        suggestion2.setText(exerciseSuggestions.get(randomNumber));
+        suggestion3 = root.findViewById(R.id.text_suggestion3);
+        randomNumber = random.nextInt(30);
+        suggestion3.setText(healthySuggestions.get(randomNumber));
 
         docRef.collection("calorieLogs").get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
